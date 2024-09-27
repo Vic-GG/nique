960b6dd3b2f34ee6933c4e1635c0f8f9b179f1b3
victorgg <137881822+Vic-GG@users.noreply.github.com>
2024-09-27 10:57:13 +0200

    Uninstall Lenis

--- JS/scroll.js
+++ JS/scroll.js
@@ -4,18 +4,3 @@ scrollContainer.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   scrollContainer.scrollLeft += evt.deltaY;
 });
-
-//Lenis
- document.addEventListener('DOMContentLoaded', () => {
-  const lenis = new Lenis({
-    duration: 1.2, 
-    easing: (t) => t, 
-    smooth: true, 
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-  requestAnimationFrame(raf);
-});
